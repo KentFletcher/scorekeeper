53cb5dae81f16feb325dfed53aae088ed77c03b2
KentFletcher <kentfletch09@gmail.com>
2021-01-20 14:43:34 -0800

    increased potential players from 2 to 4, and made it so that the winner must win by two points

--- app.js
+++ app.js
@@ -1,14 +1,26 @@
-const p1 = {
-  score: 0,
-  button: document.querySelector('#playerOneBtn'),
-  display: document.querySelector('#p1display')
-}
+const allPlayers = [
+  p1 = {
+    score: 0,
+    button: document.querySelector('#playerOneBtn'),
+    display: document.querySelector('#p1display')
+  },
+  p2 = {
+    score: 0,
+    button: document.querySelector('#playerTwoBtn'),
+    display: document.querySelector('#p2display')
+  },
+  p3 = {
+    score: 0,
+    button: document.querySelector('#playerThreeBtn'),
+    display: document.querySelector('#p3display')
+  },
+  p4 = {
+    score: 0,
+    button: document.querySelector('#playerFourBtn'),
+    display: document.querySelector('#p4display')
+  }
+]
 
-const p2 = {
-  score: 0,
-  button: document.querySelector('#playerTwoBtn'),
-  display: document.querySelector('#p2display')
-}
 
 //Initialize the score to reach to win the game
 let winningScore = 3;
@@ -25,26 +37,37 @@ playTo.addEventListener('change', function (e) {
 })
 
 //function for the player 1 and 2 buttons
-function updateScores(player, opponent) {
+function updateScores(player, ...opponent) {
   if (!isGameOver) {
     player.score += 1;
-    if (player.score === winningScore) {
+    player.display.textContent = player.score;
+    //Players must win by 2 points
+    if (player.score >= winningScore && opponent.every(opp => player.score >= opp.score + 2)) {
       isGameOver = true;
       player.display.classList.add('has-text-success');
-      opponent.display.classList.add('has-text-danger');
       player.button.disabled = true;
-      opponent.button.disabled = true;
+      opponent.forEach(opp => {
+        opp.display.classList.add('has-text-danger');
+        opponent.forEach(opp => opp.button.disabled = true);
+      })
     }
-    player.display.textContent = player.score;
+
   }
 }
 
+
 //event listeners for the buttons
 p1.button.addEventListener('click', function (e) {
-  updateScores(p1, p2)
+  updateScores(allPlayers[0], allPlayers[1], allPlayers[2], allPlayers[3])
 })
 p2.button.addEventListener('click', function (e) {
-  updateScores(p2, p1)
+  updateScores(allPlayers[1], allPlayers[0], allPlayers[2], allPlayers[3])
+})
+p3.button.addEventListener('click', function (e) {
+  updateScores(allPlayers[2], allPlayers[0], allPlayers[2], allPlayers[3])
+})
+p4.button.addEventListener('click', function (e) {
+  updateScores(allPlayers[3], allPlayers[0], allPlayers[2], allPlayers[2])
 })
 //reset all if the reset button is pressed
 resetBtn.addEventListener('click', reset)
@@ -52,7 +75,7 @@ resetBtn.addEventListener('click', reset)
 //function to reset for a fresh start
 function reset() {
   isGameOver = false;
-  for (let p of [p1, p2]) {
+  for (let p of allPlayers) {
     p.score = 0;
     p.display.textContent = p.score;
     p.display.classList.remove('has-text-success', 'has-text-danger');
