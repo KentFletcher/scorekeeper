273ecfc40c9470a8ff1cb17e806027aa18ae14d7
KentFletcher <kentfletch09@gmail.com>
2021-01-19 19:55:26 -0800

    refactored, moving reset to a function and adding a isGameOver Boolean

--- app.js
+++ app.js
@@ -15,49 +15,59 @@ let p1Score = 0;
 let p2Score = 0;
 //Initialize the score to reach to win the game
 let winningScore = 3;
+let isGameOver = false;
 
 //event listener on the winning score select of playTo
 playTo.addEventListener('change', function (e) {
-  winningScore = parseInt(e.target.value);
+  winningScore = parseInt(this.value);
+  reset();
 })
 
 //event listeners for the buttons
 playerOneBtn.addEventListener('click', function (e) {
-  p1Score += 1;
-  p1Display.textContent = p1Score;
-  if (p1Score === winningScore) {
-    p1Display.style.color = 'green';
-    p2Display.style.color = 'red';
-    playerTwoBtn.style.opacity = 0.6;
-    playerOneBtn.style.opacity = 0.6;
-    playerOneBtn.disabled = true;
-    playerTwoBtn.disabled = true;
+  if (!isGameOver) {
+    p1Score += 1;
+    p1Display.textContent = p1Score;
+    if (p1Score === winningScore) {
+      isGameOver = true;
+      p1Display.classList.add('winner');
+      p2Display.classList.add('loser');
+      playerTwoBtn.style.opacity = 0.6;
+      playerOneBtn.style.opacity = 0.6;
+      playerOneBtn.disabled = true;
+      playerTwoBtn.disabled = true;
+    }
   }
 })
 playerTwoBtn.addEventListener('click', function (e) {
-  p2Score += 1;
-  p2Display.textContent = p2Score;
-  if (p2Score === winningScore) {
-    p2Display.style.color = 'green';
-    p1Display.style.color = 'red';
-    playerTwoBtn.style.opacity = 0.6;
-    playerOneBtn.style.opacity = 0.6;
-    playerOneBtn.disabled = true;
-    playerTwoBtn.disabled = true;
+  if (!isGameOver) {
+    p2Score += 1;
+    p2Display.textContent = p2Score;
+    if (p2Score === winningScore) {
+      isGameOver = true;
+      p2Display.classList.add('winner');
+      p1Display.classList.add('loser');
+      playerTwoBtn.style.opacity = 0.6;
+      playerOneBtn.style.opacity = 0.6;
+      playerOneBtn.disabled = true;
+      playerTwoBtn.disabled = true;
+    }
   }
 })
 //reset all if the reset button is pressed
-resetBtn.addEventListener('click', function (e) {
+resetBtn.addEventListener('click', reset)
+
+//function to reset for a fresh start
+function reset() {
+  isGameOver = false;
   p1Score = 0;
   p2Score = 0;
   p2Display.textContent = p2Score;
   p1Display.textContent = p1Score;
-  p2Display.style.color = 'black';
-  p1Display.style.color = 'black';
+  p2Display.classList.remove('winner', 'loser');
+  p1Display.classList.remove('loser', 'winner');
   playerTwoBtn.style.opacity = 1;
   playerOneBtn.style.opacity = 1;
   playerOneBtn.disabled = false;
   playerTwoBtn.disabled = false;
-})
-
-
+}
